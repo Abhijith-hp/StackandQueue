b089771504e0014ab65ba2de1b9db3cc516d7b0d
abhijith_g1 <abhijith_g@geojit.com>
2025-07-22 09:14:12 +0530

    Implemented nearest small number using stack

---
+++ nearestsmallnumber.cpp
@@ -0,0 +1,20 @@
+#include<stack>
+class Solution {
+  public:
+    vector<int> leftSmaller(vector<int> arr) {
+        // code here
+        vector<int>ans;
+        std::stack<int>st;
+        
+        for(int i=0;i<arr.size();i++){
+           while(!st.empty() && st.top()>=arr[i])
+               st.pop();
+               
+           int value = st.empty() ? -1 : st.top();
+           
+           ans.push_back(value);
+           st.push(arr[i]);
+        }
+        return ans;
+    }
+};
